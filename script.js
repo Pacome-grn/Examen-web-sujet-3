b3e2634b42fc8e519735926ef30cd1b2ff7db05f
Pacome-grn <pagarnier@eleve.st-charles.eu>
2025-12-09 15:46:48 +0000

    fin du cour

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const bonneréponse1 = document.querySelector("button 2");
 const bonneréponse2 = document.querySelector("button 4");
 const bonneréponse3 = document.querySelector("button 7"); 
 
-if ("bonneréponse") {
+if ("bonneréponse1") {
+  
+  bonneréponse.addEventListener('click', () => {
+  
+    document.button.classList.toggle('dark-mode');
+  });
+}
+if ("bonneréponse2") {
+  
+  bonneréponse.addEventListener('click', () => {
+  
+    document.button.classList.toggle('dark-mode');
+  });
+}
+if ("bonneréponse3") {
   
   bonneréponse.addEventListener('click', () => {
   
